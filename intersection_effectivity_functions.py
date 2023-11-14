2214459d49d7b97028d0bfda3ab371d22f00acf2
Giacomo Labbri <giacomo.labbri@gmail.com>
2023-11-14 17:10:41 +0100

    added codec check and cleaned the code

--- intersection_effectivity_functions.py
+++ intersection_effectivity_functions.py
@@ -3,6 +3,7 @@
 import os
 import librosa
 import numpy as np
+from matplotlib import pyplot as plt
 import soundfile as sf
 
 # data augmentation (numpy and librosa)
@@ -42,3 +43,32 @@ def process_file(file, output_dir):
     # Save augmented audio to file
     sf.write(output_file_path, audio_shifted, sr)
     print("saved new file as ", output_file_path)
+
+def create_mel_specs(path_to_wav, path_to_png):
+
+    #create a list of all wav files needed to be converted: 
+    
+    file_list = [file for file in os.listdir(path_to_wav)]
+    
+    for file in file_list:    
+        
+        #create correct file name for saving
+        audio_file = os.path.join(path_to_wav, file)
+        output_file = os.path.join(path_to_png, os.path.splitext(file)[0] + '.png')
+        
+        # load the files and transfrom into np array
+        samples, sample_rate = librosa.load(audio_file, sr=44100, mono=True)
+        
+        #apply a stft transformation    
+        stft = librosa.stft(samples)
+
+        #modify it in order to use the mel-scale instead of frenquency
+        mel_scale_spec, par = librosa.magphase(stft)
+        mel_spec = librosa.feature.melspectrogram(S=mel_scale_spec, sr=sample_rate)     
+        
+        # Create a figure with the specified size and aspect ratio
+        plt.figure(figsize=(2.93, 2.93))  # Adjust the scaling factor based on your preference
+        librosa.display.specshow(librosa.amplitude_to_db(mel_spec, ref=np.max), x_axis='time', y_axis='mel', sr=sample_rate)
+        plt.axis('off')
+        plt.savefig(output_file, bbox_inches='tight', pad_inches=0, transparent=True)
+        plt.close()
